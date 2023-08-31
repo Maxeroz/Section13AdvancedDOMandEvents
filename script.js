3eb04ecece5438216dde10440775338d95c11d41
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-08-31 17:39:35 +0400

    Event Propagation in Practice

--- script.js
+++ script.js
@@ -159,6 +159,7 @@ btnScrollTo.addEventListener('click', function (e) {
 ///////////////////////////////////////
 // Types of Events and Event Handlers
 
+/*
 const h1 = document.querySelector('h1');
 
 const alertH1 = function (e) {
@@ -174,6 +175,36 @@ h1.addEventListener('mouseenter', alertH1);
 // };
 
 // console.log(h1);
+*/
 
 ///////////////////////////////////////
 // Event Propagation: Bubbling and Capturing
+
+// Event Propagation in Practice
+
+// rgb(255,255,255)
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  // e.preventDefault();
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+  // Stop propagation
+  // e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  // e.preventDefault();
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  // e.preventDefault();
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+});
